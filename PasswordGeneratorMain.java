dbdfb06312d546f150f92a935c8e10b681232db9
nicolas-1324 <nicolaspucer@gmail.com>
2025-02-16 22:13:52 +0100

    basic password generator, updat later

--- PasswordGeneratorMain.java
+++ PasswordGeneratorMain.java
@@ -1,5 +1,14 @@
+import java.lang.StringBuilder;
+
 public class PasswordGeneratorMain{
 	public static void main(String[] args){
-		
+		StringBuilder niz = new StringBuilder();
+		//Zgeneriraj random stevilko med 33 in 126 vkljucno
+		for(int i = 0; i<15; i++){
+			int randSt = (int)(Math.random() * 126-33+1)+33;
+			char randZnak = (char) randSt;
+			niz = niz.append(randZnak);
+		}
+		System.out.println("Your generated password is: " + niz);
 	}
 }
